4bfda6403ff2720a87a0272c9693ac8609fc2a03
Oluwafikunayomi <mailcoding01@gmail.com>
2026-06-24 15:03:00 +0100

    added cardDeal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className="{`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}">
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} 
+    rounded-[10px] mt-10}`}>
       Get Started
     </button>
   )
