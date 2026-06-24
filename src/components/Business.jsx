289aaba343850df1f8d77c73af093e06e50dff06
Oluwafikunayomi <mailcoding01@gmail.com>
2026-06-24 12:50:45 +0100

    updating business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,19 +1,36 @@
 import { features } from '../constants';
-import styles from '../styles';
+import styles, { layout } from '../styles';
+import Button from './Button';
+
+const FeatureCard =({ icon, title, content, index }) => (
+  <div>
+    <div>
+      <img src={icon} alt='icon' />
+    </div>
+  </div>
+)
+
 
 const Business = () => (
-    <section id='features' className={layout.section}>
+    <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>You do the business, <br 
-        className='sm:block hidden' /> we'll 
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we'll 
         handle the money.</h2>
-        <p className={`${styles.paragraph} 
-        max-w-[470px] mt-5`}>With the right 
-        credit card, you can improve your 
-        financial life by building credit, 
-        earning rewards and saving money. But 
-        with hundreds of credit cards on the 
-        market.</p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can 
+          improve your financial life by building 
+          credit, earning rewards and saving 
+          money. But with hundreds of credit 
+          cards on the market.
+        </p>
+
+        <Button styles='mt-10' />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} />
+        ))}
       </div>
     </section>
   )
